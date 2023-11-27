2eb631607808578db46fa0337406ff9b12125a28
ayubyogabfi <109940825+ayubyogabfi@users.noreply.github.com>
2023-11-27 08:10:27 +0700

    feat(learning-service) : Add API Get Article List

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/article")
 public class ArticleController {
 
     private final ArticleService articleService;
