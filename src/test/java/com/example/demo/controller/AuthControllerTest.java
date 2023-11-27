890e5fa7fb6dc0efdd5dc853c07b8357609d6f21
ayubyogabfi <109940825+ayubyogabfi@users.noreply.github.com>
2023-11-27 16:01:48 +0700

    feat(learning-service) : Add API Get Article By Keyword

--- src/test/java/com/example/demo/controller/AuthControllerTest.java
+++ src/test/java/com/example/demo/controller/AuthControllerTest.java
@@ -40,9 +40,6 @@ class AuthControllerTest {
     @MockBean
     private UserService userService;
 
-    /**
-     * Method under test: {@link AuthController#login(LoginRequest)}
-     */
     @Test
     void testLogin() throws Exception {
         when(authenticationManager.authenticate(Mockito.<Authentication>any()))
@@ -59,59 +56,9 @@ class AuthControllerTest {
         actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
     }
 
-    /**
-     * Method under test: {@link AuthController#login(LoginRequest)}
-     */
-    @Test
-    void testLogin2() throws Exception {
-        when(authenticationManager.authenticate(Mockito.<Authentication>any()))
-                .thenThrow(new BadCredentialsException("?"));
-
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setPassword("iloveyou");
-        loginRequest.setUsername("janedoe");
-        String content = (new ObjectMapper()).writeValueAsString(loginRequest);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/v1/authenticate/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(authController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(401));
-    }
-
-    /**
-     * Method under test: {@link AuthController#refreshToken(HttpServletRequest, HttpServletResponse, Authentication)}
-     */
     @Test
     void testRefreshToken() throws Exception {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/v1/authenticate/refresh-token");
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(authController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(400));
-    }
-
-    /**
-     * Method under test: {@link AuthController#refreshToken(HttpServletRequest, HttpServletResponse, Authentication)}
-     */
-    @Test
-    void testRefreshToken2() throws Exception {
-        SecurityMockMvcRequestBuilders.FormLoginRequestBuilder requestBuilder = SecurityMockMvcRequestBuilders
-                .formLogin();
-        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(authController)
-                .build()
-                .perform(requestBuilder);
-        actualPerformResult.andExpect(MockMvcResultMatchers.status().isNotFound());
-    }
-
-    /**
-     * Method under test: {@link AuthController#refreshToken(HttpServletRequest, HttpServletResponse, Authentication)}
-     */
-    @Test
-    void testRefreshToken3() throws Exception {
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/v1/authenticate/refresh-token");
-        requestBuilder.characterEncoding("Encoding");
         ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(authController)
                 .build()
                 .perform(requestBuilder);
