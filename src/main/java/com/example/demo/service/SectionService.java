99809e5c988f4873e3d4c22b99df3e4e3939a851
ayubyogabfi <109940825+ayubyogabfi@users.noreply.github.com>
2023-12-06 17:44:03 +0700

    fix(learning-service) : fixing issue on fetching data by user log in

--- src/main/java/com/example/demo/service/SectionService.java
+++ src/main/java/com/example/demo/service/SectionService.java
@@ -4,5 +4,5 @@ import com.example.demo.entity.Section;
 import java.util.List;
 
 public interface SectionService {
-  List<Section> getAllSections(String token);
+  List<Section> getAllSections(String extractedUsername);
 }
